7963e1808395f302ace3119585d5f6739455e477
Gulam Qasim <gfram@hotmail.co.uk>
2020-05-10 09:26:43 +0100

    Updated JavaScript

---
+++ JavaScript/Lesson 2 - Variables/bin.js
@@ -0,0 +1,22 @@
+//var allows us to store content
+var age = 40;
+
+// creates a string without a value
+var firstname;
+
+// assigns the value to the name
+firstname = "Gulam";
+
+// you can also change a value at any time
+firstname = "Changed again"
+
+// signals in javaScript a tab in that position
+firstname = 'this is a tabbed \t text';
+
+// Creates a new line
+firstname = 'this is \n a new line';
+
+//Boolean is either true or false
+
+// calls our "age" content
+alert(firstname);
